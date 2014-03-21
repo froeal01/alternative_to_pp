1dfe4161216275711a99f12958fac31b40af9287
Alex Froelich <alexfroelich@Alexs-MacBook-Pro.local>
2014-03-21 10:48:23 -0400

    deleted extra jquery

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,9 @@ Presentation::Application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
+
+  config.assets.precompile += ['screen.css', 'global.js', 'jquery.presentation.js']
 
   # Generate digests for assets URLs.
   config.assets.digest = true
